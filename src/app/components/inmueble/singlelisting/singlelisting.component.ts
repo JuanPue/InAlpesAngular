21dfd2458f3cc0211629a62146503225ca6db2c3
JuanPue <83104887+JuanPue@users.noreply.github.com>
2021-04-24 22:09:01 -0500

    falta de archivo

--- src/app/components/inmueble/singlelisting/singlelisting.component.ts
+++ src/app/components/inmueble/singlelisting/singlelisting.component.ts
@@ -12,15 +12,12 @@ export class SinglelistingComponent implements OnInit {
   id;
   constructor(private activatedroute:ActivatedRoute, private router:Router, private inmuebleService:InmuebleServiceService) {
     this.activatedroute.params.subscribe(data => {
-      console.log(data);
       this.id = data;
-      console.log(this.id);
     })
    }
 
   ngOnInit(): void {
     console.log(this.id);
-
   }
 
 }
